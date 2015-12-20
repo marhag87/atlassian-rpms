688487e3bcdaaaa7904fb9c27546f95f2769ba47
Martin Hagstrom <marhag87@gmail.com>
2015-12-20 09:32:44 +0100

    Change makefile into a bash script
    I don't know enough about make to get it to do the things I want.

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function version() {
+  awk '/^Version/ {print $2}' SPECS/$1.spec
+}
+
+function build() {
+	rm -rf BUILD BUILDROOT RPMS SRPMS || true
+  test -s SOURCES/atlassian-$1-$(version $1).tar.gz || wget https://downloads.atlassian.com/software/${2:-$1}/downloads/atlassian-${3}$1-$(version $1).tar.gz -O SOURCES/atlassian-$1-$(version $1).tar.gz
+  rpmbuild --define "_topdir $(git rev-parse --show-toplevel)" -bb SPECS/$1.spec
+  mv RPMS/x86_64/$1-*.rpm .
+}
+
+if [[ -z $1 ]]; then
+  $0 bamboo
+  $0 bitbucket
+  $0 confluence
+  $0 jira
+else
+  case $1 in
+    jira)
+      build jira jira "jira-software-$(version jira)-"
+      ;;
+    bitbucket)
+      build bitbucket stash
+      ;;
+    *)
+      build $1
+      ;;
+  esac
+fi
